bc63c40dddfc7aed241db117ccad9ae9137d099f
MrYaz <LehaHotPlay@yandex.ru>
2024-06-17 20:32:52 +0500

    Добавленны коментарии

--- main.cpp
+++ main.cpp
@@ -10,33 +10,33 @@ void CheckPrimeNumber(unsigned); // Прототип функции CheckPrimeNu
 
 int main()
 {
-    unsigned inputNumber { };
+    unsigned inputNumber { }; 
 
     cout << endl; // Отступ
     cout << "Введите число для проверки простое ли оно: ";
     cin >> inputNumber;
 
-    CheckPrimeNumber(inputNumber);
+    CheckPrimeNumber(inputNumber); // Проверяем вводимое число на простоту
 }
 
 void CheckPrimeNumber(unsigned number)
 {
-    if (number == 1)
+    if (number == 1) // Число 1 не простое ни стоставное
     {
         cout << endl; // Отступ
         cout << "Число: " << number << " ни простое и не составное" << endl;
     }
-    else if (number % 2 == 0)
+    else if (number % 2 == 0) // Если число делится на 2 без остатка, оно не простое (лично по моим наблюдениям)
     {  
         cout << endl; // Отступ
         cout << "Число: " << number << " не является простым" << endl;
     }
-    else if (number % 2 != 0)
+    else if (number % 2 != 0) // Если число не делится на 2, оно простое (лично по моим наблюдениям)
     {
         cout << endl; // Отступ
         cout << "Число: " << number << " является простым" << endl;
     }
-    else 
+    else // Если число и не простое и не составное
     {
         cout << endl; // Отступ
         cout << "Вы ввели что-либо неправильно." << endl;
